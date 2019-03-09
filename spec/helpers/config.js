5d818d17f7f97cb53b4c8820f22f5d2240fb174e
Peter Ullrich <p.ullrich@dotup.de>
2019-03-09 10:27:07 +0100

    tests moved to mocha

--- spec/helpers/config.js
+++
@@ -1,5 +0,0 @@
-var TSConsoleReporter = require("jasmine-ts-console-reporter");
-var jasmineReporters = require("jasmine-reporters");
-jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(new TSConsoleReporter());
-//# sourceMappingURL=config.js.map
