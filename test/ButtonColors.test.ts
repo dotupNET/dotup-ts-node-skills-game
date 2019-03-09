b0ccda99ff4671c77061d89e83a3a4a39a8ee3fa
Peter Ullrich <p.ullrich@dotup.de>
2019-03-09 10:33:29 +0100

    tests

--- test/ButtonColors.test.ts
+++ test/ButtonColors.test.ts
@@ -1,3 +1,6 @@
+// tslint:disable: newline-per-chained-call
+// tslint:disable-next-line: no-implicit-dependencies
+import { expect } from 'chai';
 import { ButtonColor, ButtonColors } from '../src';
 
 describe('ButtonColors', () => {
@@ -5,24 +8,19 @@ describe('ButtonColors', () => {
   it('should get two colors', () => {
 
     const names = ButtonColors.getRandomColorNames(2);
-    expect(names.length)
-      .toBe(2);
+    expect(names.length).equals(2);
 
     const colors = ButtonColors.getRandomColors(2);
-    expect(colors.length)
-      .toBe(2);
+    expect(colors.length).equals(2);
 
     const value = ButtonColor.red;
     const convertet = ButtonColors.getColorName(value);
-    expect(convertet === 'red')
-      .toBeTruthy();
+    expect(convertet).equals('red');
 
     const allColors = ButtonColors.getAllColors();
-    expect(allColors.length)
-      .toBe(11);
+    expect(allColors.length).equals(11);
 
-    expect(allColors[0] === ButtonColor.white)
-      .toBeTruthy();
+    expect(allColors[0]).equals(ButtonColor.white);
 
   });
 
